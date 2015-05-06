4b35ade40a64b36e5cfd40686321da91f59d51bb
Archana Chandrasekharan <kcarchana77@gmail.com>
2015-05-06 19:02:14 +0530

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,54 @@
+def quickSort(alist):
+   quickSortHelper(alist,0,len(alist)-1)
+
+def quickSortHelper(alist,first,last):
+   if first<last:
+
+       splitpoint = partition(alist,first,last)
+
+       quickSortHelper(alist,first,splitpoint-1)
+       quickSortHelper(alist,splitpoint+1,last)
+
+
+def partition(alist,first,last):
+   pivotvalue = alist[first]
+
+   leftmark = first+1
+   rightmark = last
+
+   done = False
+   while not done:
+
+       while leftmark <= rightmark and \
+               alist[leftmark] <= pivotvalue:
+           leftmark = leftmark + 1
+
+       while alist[rightmark] >= pivotvalue and \
+               rightmark >= leftmark:
+           rightmark = rightmark -1
+
+       if rightmark < leftmark:
+           done = True
+       else:
+           temp = alist[leftmark]
+           alist[leftmark] = alist[rightmark]
+           alist[rightmark] = temp
+
+   temp = alist[first]
+   alist[first] = alist[rightmark]
+   alist[rightmark] = temp
+
+
+   return rightmark
+
+nterms = int(input("How many terms? "))
+list = []
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Quick sort")
+   for i in range(nterms):
+      list[i]
+      i++
+      print(quickSort(list))
+print(list)
